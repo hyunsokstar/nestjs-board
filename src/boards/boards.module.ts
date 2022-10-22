eda764bce93375fda830a17d5e344dcf6fc61049
hyun <hyunsokstar@gmail.com>
2022-10-22 18:31:17 +0900

    update

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { DatabaseModule } from '../database/database.module';
 
 
 @Module({
+  imports: [DatabaseModule],
+
   controllers: [BoardsController],
   providers: [BoardsService]
 })
+
 export class BoardsModule {}
