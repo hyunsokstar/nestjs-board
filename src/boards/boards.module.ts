3cc64675f8d617a93bf220abb23552954604fd35
hyun <hyunsokstar@gmail.com>
2022-10-22 18:32:57 +0900

    update for providers

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { DatabaseModule } from '../database/database.module';
-
+import { boardProviders } from './board.provider';
 
 @Module({
   imports: [DatabaseModule],
 
   controllers: [BoardsController],
-  providers: [BoardsService]
+  providers: [...boardProviders, BoardsService],
 })
-
 export class BoardsModule {}
