e96b2f2a17f71abccb7c77a3027b2d9698945b3e
hyun <hyunsokstar@gmail.com>
2022-10-22 18:16:32 +0900

    update for 1022

---
+++ src/boards/board.repository.ts
@@ -0,0 +1,9 @@
+import {Board as BoardEntity} from "../database/board.entity"
+
+
+export class BoardRepository {
+    // constructor(
+    //     @InjectRepository(BoardEntity)
+    //     private readonly boardRepository: Repository(BoardEntity)
+    // )
+}
